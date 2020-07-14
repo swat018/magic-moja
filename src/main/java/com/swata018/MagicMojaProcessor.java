4b1f4e64ae46cfed79664a9ed8329a29a57951fd
jinwoo park <swat018@gmail.com>
2020-07-14 14:19:55 +0900

    애노테이션 프로세서 (Filer 인터페이스)

--- src/main/java/com/swata018/MagicMojaProcessor.java
+++ src/main/java/com/swata018/MagicMojaProcessor.java
@@ -1,17 +1,20 @@
 package com.swata018;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import com.sun.source.tree.Tree;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.Name;
-import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.*;
 import javax.tools.Diagnostic;
+import java.io.IOException;
 import java.util.Set;
 
 @AutoService(Processor.class)
@@ -37,6 +40,31 @@ public class MagicMojaProcessor extends AbstractProcessor {
             } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Processing " + simpleName);
             }
+
+            TypeElement typeElement = (TypeElement)element;
+            ClassName className = ClassName.get(typeElement);
+
+            MethodSpec pullOut = MethodSpec.methodBuilder("pullOut")
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(String.class)
+                    .addStatement("return $S", "Rabbit!")
+                    .build();
+            TypeSpec MagicMoja = TypeSpec.classBuilder("MagicMoja")
+                    .addModifiers(Modifier.PUBLIC)
+                    .addSuperinterface(className)
+                    .addMethod(pullOut)
+                    .build();
+
+            Filer filer = processingEnv.getFiler();
+
+            try {
+                JavaFile.builder(className.packageName(), MagicMoja)
+                        .build()
+                        .writeTo(filer);
+            } catch (IOException e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "FATAL ERROR: " + e);
+            }
+
         }
         return true;
 
